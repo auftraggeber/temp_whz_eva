ece5946f09ba9af4b8f46751a9bb99a3d8d4b110
Jonas Langner <jonas.langner.kuz@fh-zwickau.de>
2021-10-18 16:36:50 +0200

    Basisimplementation.

--- src/main/java/whz/pti/eva/praktikum_02/grade/domain/Grade.java
+++ src/main/java/whz/pti/eva/praktikum_02/grade/domain/Grade.java
@@ -1,5 +1,28 @@
 package whz.pti.eva.praktikum_02.grade.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Grade {
 
+	@Id
+	@GeneratedValue
+	private int id;
+	private String lecture, grade;
+	
+	public Grade(String lecture, String grade) {
+		this.lecture = lecture;
+		this.grade = grade;
+	}
+	
+	public String getLecture() {
+		return lecture;
+	}
+	
+	public String getGrade() {
+		return grade;
+	}
+	
 }
